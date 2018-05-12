fead6259f877cde54f0d444532c1d6cce38b09d1
Vladbelka <vladbelka97@gmail.com>
2018-05-12 14:27:38 +0300

    Personal code display (Invite code)

--- app/src/main/java/beloshitskiy/kidcontroltracker/InviteCodeActivity.java
+++ app/src/main/java/beloshitskiy/kidcontroltracker/InviteCodeActivity.java
@@ -1,13 +1,38 @@
 package beloshitskiy.kidcontroltracker;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class InviteCodeActivity extends AppCompatActivity {
+
+
+    String name, email, password, date, issharing, code;
+    Uri imageUri;
+
+    TextView t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_code);
+        t1 = (TextView)findViewById(R.id.textView);
+
+        Intent myIntent = getIntent();
+        if(myIntent!=null)
+        {
+            name = myIntent.getStringExtra("name");
+            email = myIntent.getStringExtra("email");
+            password = myIntent.getStringExtra("password");
+            code = myIntent.getStringExtra("code");
+            issharing = myIntent.getStringExtra("isSharing");
+            imageUri = myIntent.getParcelableExtra("imageUri");
+
+        }
+        t1.setText(code);
+
     }
+
 }
