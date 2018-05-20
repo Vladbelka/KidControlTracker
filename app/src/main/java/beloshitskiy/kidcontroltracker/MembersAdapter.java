77f54fa1868c70d7f90bbe3ba697f7305f1cc515
Vladbelka <vladbelka97@gmail.com>
2018-05-20 11:59:54 +0300

    Displaying members in group

---
+++ app/src/main/java/beloshitskiy/kidcontroltracker/MembersAdapter.java
@@ -0,0 +1,81 @@
+package beloshitskiy.kidcontroltracker;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MembersAdapter extends RecyclerView.Adapter<MembersAdapter.MembersViewHolder>
+{
+    ArrayList<CreateUser> nameList;
+    Context c;
+
+    MembersAdapter(ArrayList<CreateUser> nameList, Context c)
+    {
+        this.nameList = nameList;
+        this.c = c;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return nameList.size();
+    }
+
+    @NonNull
+    @Override
+    public MembersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_layout, parent, false);
+        MembersViewHolder membersViewHolder = new MembersViewHolder(v,c,nameList);
+        return membersViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MembersViewHolder holder, int position) {
+
+        CreateUser currentUserObj = nameList.get(position);
+        holder.name_txt.setText(currentUserObj.name);
+        Picasso.get().load(currentUserObj.imageUrl).placeholder(R.drawable.default_profile).into(holder.circleImageView);
+    }
+
+    public static class MembersViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    {
+        TextView name_txt;
+        CircleImageView circleImageView;
+        View v;
+        Context c;
+        ArrayList<CreateUser> nameArrayList;
+        FirebaseAuth auth;
+        FirebaseUser user;
+
+        public MembersViewHolder(View itemView, Context c, ArrayList<CreateUser> nameArrayList) {
+            super(itemView);
+            this.c = c;
+            this.nameArrayList = nameArrayList;
+
+            itemView.setOnClickListener(this);
+            auth = FirebaseAuth.getInstance();
+            user = auth.getCurrentUser();
+
+            name_txt = itemView.findViewById(R.id.item_title);
+            circleImageView = itemView.findViewById(R.id.i11);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(c, "You have selected this user", Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/beloshitskiy/kidcontroltracker/MyGroupActivity.java
@@ -0,0 +1,86 @@
+package beloshitskiy.kidcontroltracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MyGroupActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    CreateUser createUser;
+    ArrayList<CreateUser> nameList;
+    DatabaseReference reference, userReference;
+    String groupMemberId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_group);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerview);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        nameList = new ArrayList<>();
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        userReference = FirebaseDatabase.getInstance().getReference().child("Users");
+        reference = FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid()).child("GroupMembers");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                nameList.clear();
+                if(dataSnapshot.exists())
+                {
+                    for (DataSnapshot dss: dataSnapshot.getChildren())
+                    {
+                        groupMemberId = dss.child("groupMemberId").getValue(String.class);
+                        userReference.child(groupMemberId)
+                                .addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        createUser = dataSnapshot.getValue(CreateUser.class);
+                                        nameList.add(createUser);
+                                        adapter.notifyDataSetChanged();
+                                    }
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+                                        Toast.makeText(getApplicationContext(),databaseError.getMessage(),Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(),databaseError.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        adapter = new MembersAdapter(nameList, getApplicationContext());
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+    }
+}
